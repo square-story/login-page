5562f2c22382346f6effeb4eed6de4f17635da50
square-story <gibmepreo@gmail.com>
2024-04-16 14:39:24 +0530

    problem figure-out

--- router.js
+++ router.js
@@ -23,26 +23,32 @@ router.post('/login',(req,res)=>{
 //login successful and will redirect to dashboard
 router.get('/dashboard',(req,res)=>{
     if (req.session.user) {
-        //success message passed
+        //username message passed
         res.render('dashboard',{user:req.session.user});
-        req.session.destroy(); // Destroy the session
+        // req.session.destroy(); // Destroy the session
     }else{
         res.send("login timeout");
     }
 })
 //route of logout
 router.get('/logout',(req,res)=>{
-    req.session.destroy(function (err) {
+    req.session.destroy((err) => {
         if (err) {
-            
             console.log(err);
             res.send("error")
+            return res.redirect('/'); // Handle error gracefully (e.g., display error message)
+            
         }else{
             // res.redirect('/?invaild=true')
             res.render('base',{title:"express",logout:"logout successfully.."})
         }
     })
 })
+
+// app.get('/logout', (req, res) => {
+//     res.render('logout', { message: 'You have been logged out.' });
+//     // Add JavaScript code to prevent back button (see below)
+//   });
 //exports the router
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,34 +3,34 @@ const path = require('path');
 const app = express();
 const bodyparser = require('body-parser');
 const session = require('express-session');
-const nocache = require('nocache');
+const cookieParser = require('cookie-parser');
 //uuid creation for generation of token
 const {v4:uuidv4} = require('uuid');
 //router path setting
 const route = require('./router')
-app.use(nocache());
+app.use(cookieParser());
 app.set("view engine",'ejs');
 //load static assets
 app.use('/static',express.static(path.join(__dirname,'public')))
 //insert background
 app.use('/assets',express.static(path.join(__dirname,'public/assets')))
-app.use('/images',express.static(path.join(__dirname,'views/images')))
 //use body parser
 app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({extended:false}))
 //use session module
 app.use(session({
     secret:uuidv4(), //to create secret keys and unique
-    resave:false,
-    saveUninitialized:true
+    resave:false,  // Set to true to force the session to be saved back to the session store
+    saveUninitialized:true, // Set to true to save uninitialized sessions
+    cookie: { secure: false } // Set to 'true' for HTTPS in production
 }));
 //route use app.use
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.use('/route',route)
 //home route
 app.get("/",(req,res)=>{
     res.render("base",{title:"login system"});
 })
 app.listen(port,()=>{
-    console.log("server is running on http://localhost:3000");
+    console.log("server is running on http://localhost:8000");
 });
